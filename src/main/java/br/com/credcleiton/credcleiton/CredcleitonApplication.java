1bdd72552b88fcdfb0bcafa1c6ec6e0ea58bb132
RafaelVendrameto <rafaelvendrameto@gmail.com>
2020-02-04 10:32:18 -0300

    criar banco de dados

---
+++ src/main/java/br/com/credcleiton/credcleiton/CredcleitonApplication.java
@@ -0,0 +1,13 @@
+package br.com.credcleiton.credcleiton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredcleitonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredcleitonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/credcleiton/credcleiton/CredcleitonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.credcleiton.credcleiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredcleitonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
