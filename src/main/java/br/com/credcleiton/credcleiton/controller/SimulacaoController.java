4a3fab3de80d19e78183ae7d8bc4374d35176b46
Kauan Victor <k1one.g.vitor@gmail.com>
2020-02-05 11:56:09 -0300

    acrescentar pagina index

--- src/main/java/br/com/credcleiton/credcleiton/controller/SimulacaoController.java
+++ src/main/java/br/com/credcleiton/credcleiton/controller/SimulacaoController.java
@@ -1,4 +1,4 @@
-package br.com.credcleiton.credcleiton.Controller;
+package br.com.credcleiton.credcleiton.controller;
 
 import br.com.credcleiton.credcleiton.model.Banco;
 import br.com.credcleiton.credcleiton.model.DadosSimulacao;
@@ -9,23 +9,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SimulacaoController {
     @Autowired
     private SimulacaoService simulacaoService;
 
-    @GetMapping
-    public String simulador(){
-        return "simulador";
-    }
 
-    @PostMapping("/resposta")
-    public String listarBancos(@ModelAttribute DadosSimulacao dadosSimulacao, Model model){
-        Iterable<Banco> banco = simulacaoService.listarBancos(dadosSimulacao);
-        model.addAttribute("bancos", banco);
-        return "resposta";
+    @Controller
+    public class TesteController {
+        @GetMapping
+        public String mostrarHome() {
+            return "index";
+        }
+
+
+        @GetMapping
+        public String simulador() {
+            return "simulador";
+        }
+
+        @PostMapping("/resposta")
+        public String listarBancos(@ModelAttribute DadosSimulacao dadosSimulacao, Model model) {
+            Iterable<Banco> banco = simulacaoService.listarBancos(dadosSimulacao);
+            model.addAttribute("bancos", banco);
+            return "resposta";
+
+        }
     }
 }
