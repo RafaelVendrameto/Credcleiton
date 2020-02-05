ad69750ea4b5d4b5811d50735389ea5a561450ae
RafaelVendrameto <rafaelvendrameto@gmail.com>
2020-02-05 11:55:57 -0300

    modificar repository

--- src/main/java/br/com/credcleiton/credcleiton/repository/SimulacaoRepository.java
+++ src/main/java/br/com/credcleiton/credcleiton/repository/SimulacaoRepository.java
@@ -3,9 +3,7 @@ package br.com.credcleiton.credcleiton.repository;
 import br.com.credcleiton.credcleiton.model.Banco;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
 
 public interface SimulacaoRepository extends CrudRepository<Banco, String> {
     Iterable<Banco> findByTipoEmprestimo(String nome);
-    Iterable<Banco> findByTaxa(double taxa);
 }
